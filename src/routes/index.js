210eee5c4bca73545c813cb2acf4a284c2ea8810
techeca <jp.vasquez@outlook.com>
2022-07-05 10:59:44 -0400

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
 router.get('/categorias', (req, res) => {
   //if(isNaN(req.params) & !isNaN(req.params)){console.log('true?')};
       //if(req.params) throw 'No debe entragar parametros en categorias';
-      pool.query('SELECT * FROM category', (err, rows, fields) => {
+      pool.execute('SELECT * FROM category', (err, rows, fields) => {
       if(err) err;
        const categorias = rows;
        res.json({categorias});
@@ -35,7 +35,7 @@ router.get('/categorias', (req, res) => {
 router.get('/productos/:name?', (req, res) => {
     const name = req.params.name;
     if(!req.params.name) throw 'No hay parametros para busqueda';
-    pool.query('SELECT * FROM product WHERE name LIKE ?', [`%${name}%`], (err, rows, fields) => {
+    pool.execute('SELECT * FROM product WHERE name LIKE ?', [`%${name}%`], (err, rows, fields) => {
      if(err) throw err;
       const productos = rows
       res.json({productos})
@@ -58,9 +58,9 @@ router.get('/productosByCategoria?', async (req, res, next) => {
     if(isNaN(page) || page < 0) throw `Parametro 'page' incorrecto... ej: /?id=1&page=1`;
       //totalProductos es para saber cuantos productos hay y poder calcular cuantos botones de paginas hay que generar
       //Al relizar 2 consultas 1 puede no llegar a tiempo y enviar valores en blanco, para eso asyn/await
-      let [rows, fields] = await promisePool.query(`SELECT COUNT(*) as totalProductos FROM product WHERE category = ?`, [id]);
+      let [rows, fields] = await promisePool.execute(`SELECT COUNT(*) as totalProductos FROM product WHERE category = ?`, [id]);
       totalProductos = rows;
-        pool.query(`SELECT * FROM product WHERE category = ? LIMIT ${limite} OFFSET ${page * limite}`, [id], (err, rows, fields) => {
+        pool.execute(`SELECT * FROM product WHERE category = ? LIMIT ${limite} OFFSET ${page * limite}`, [id], (err, rows, fields) => {
           if(err) throw err;
           res.json({productos:rows, total:totalProductos});
         })
